228ee614653eed379d201219df844c53f910b921
jagannathking <sethij832@gmail.com>
2025-04-09 09:36:56 +0530

    cors debug

--- server/index.js
+++ server/index.js
@@ -1,28 +1,36 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth.routes.js"; 
+import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js"
 import cors from "cors"
 
 
 dotenv.config();
-const app=express();
+const app = express();
 
-app.use(express.json());
 connectDB();
 
+
+app.use(express.json());
 app.use(cors());
-  
-  app.get("/", (req, res) => {
-    res.json({ message: "Hello welcome to backend" });
-  });
-    
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Healthy" });
+});
+
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-const port =process.env.PORT;
 
-app.listen(port,()=>{
-    console.log(`server connected on PORT ${port}`)
+
+const port = process.env.PORT;
+
+//server
+app.listen(port, () => {
+
+  console.log(`server connected on PORT ${port}`)
 });
